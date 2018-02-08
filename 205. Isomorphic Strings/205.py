de348c3669c97e8237e51f728fbcc9357818e304
TongTTT666 <35908560+TongTTT666@users.noreply.github.com>
2018-02-08 12:37:46 +0800

    update 205

---
+++ 205. Isomorphic Strings/205.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isIsomorphic(self, s, t):
+        # Hash Table的一个实例，用字典去解决对应关系的问题
+        hashTable = {} # 声明字典
+        # 先构建哈希表，然后再查，我觉得肯定是慢的
+        for i in range(len(s)):
+            if s[i] not in hashTable:
+                hashTable[s[i]] = t[i]
+        
+        for i in range(len(t)):
+            if hashTable[s[i]] != t[i]:
+                return False
+        
+        # 把s和t反过来再做一次，要防止出现重复元素对应的情况
+        s, t = t, s
+        hashTable = {} # 声明字典
+            
+        for i in range(len(s)):
+            if s[i] not in hashTable:
+                hashTable[s[i]] = t[i]
+        
+        for i in range(len(t)):
+            if hashTable[s[i]] != t[i]:
+                return False
+            
+        return True
+        
+    
