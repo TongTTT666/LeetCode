c8beeb0d27d6717d419a04f0a691a920c3e4b03f
TongTTT666 <35908560+TongTTT666@users.noreply.github.com>
2018-02-13 09:59:46 +0800

    update 652

---
+++ 652. Find Duplicate Subtrees/652.py
@@ -0,0 +1,28 @@
+# 使用哈希表
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def findDuplicateSubtrees(self, root):
+        def preTra(node):  # node是当前节点，tree是字典用来保存树结构
+            if not node:
+                return 'null'
+            structure = "%s,%s,%s" % (str(node.val), preTra(node.left),
+                                      preTra(node.right))
+            tree[structure].append(node)
+            return structure
+
+        tree = collections.defaultdict(list)
+        preTra(root)
+
+        # 重复的只用读取一个就可以了
+        return [
+            tree[structure][0] for structure in tree
+            if len(tree[structure]) > 1
+        ]
